42e7fac2c14f7269c74cdba8731d0439b9db2014
esterle <esterle@fortiss.org>
2017-09-09 18:15:24 +0200

    [Con] changed pid_throttle
    pid_throttle new is bounded in [-1,1], later gets converted to max
    braking torque

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,15 +1,20 @@
-GAS_DENSITY = 2.858
-ONE_MPH = 0.44704
 import pid
 import lowpass
 
+GAS_DENSITY = 2.858
+ONE_MPH = 0.44704
+
 
 class Controller(object):
-    def __init__(self, controller_rate, accel_limit, decel_limit, max_steer_angle):
+    def __init__(self, controller_rate, accel_limit, decel_limit, max_steer_angle, vehicle_mass, wheel_radius):
 
         self.controller_rate = controller_rate
+        self.accel_limit = accel_limit
+        self.decel_limit = decel_limit
+        self.vehicle_mass
+
         # TODO: find good parameters for PID controllers
-        self.pid_throttle = pid.PID(kp=1.0, ki=0.0, kd=0.0, mn=decel_limit, mx=accel_limit)
+        self.pid_throttle = pid.PID(kp=1.0, ki=0.0, kd=0.0, mn=-1, mx=1)
         self.pid_steer = pid.PID(kp=1.0, ki=0.0, kd=0.0, mn=-max_steer_angle, mx=max_steer_angle)
 
         self.filter_throttle = lowpass.LowPassFilter(tau=0.1, ts=1)
@@ -17,9 +22,10 @@ class Controller(object):
 
     def control(self, speed_error, cross_track_error):
         """
-
+        calculates control signal
         :param speed_error:
-        :param cross_track_error: The cross track error (cte) is the current y position of the vehicle
+        :param cross_track_error: The cross track error (cte) is the current y
+                position of the vehicle
         :return: throttle [1], brake [Nm], steer [rad]
         """
         sample_time = self.controller_rate
@@ -30,20 +36,30 @@ class Controller(object):
         throttle = self.pid_throttle.step(error=speed_error, sample_time=sample_time)
         throttle_filtered = self.filter_throttle.filt(throttle)
 
-        throttle_out = max(0, throttle_filtered)
-        brake_out = min(0, throttle_filtered) # TODO: brake_out should be in [Nm]
+        throttle_out = max(0, throttle_filtered) # TODO: does acceleration limit needs to be enforced here?
+        brake_out = - min(0, throttle_filtered) * self.decel_limit * self.vehicle_mass * wheel_radius
         steering_out = steering_angle_filtered
 
         return throttle_out, brake_out, steering_out
 
     def reset(self):
+        """
+        resets pid controllers
+        """
         self.pid_throttle.reset()
-        self.pid_brake.reset()
         self.pid_steer.reset()
 
 
 def test_controller():
-    controller = Controller(accel_limit=10, decel_limit=-10, max_steer_angle=0.25)
+    """
+    simple test of functionality by creating Controller object and performing
+    one control step
+    """
+    controller = Controller(controller_rate=30,
+                            accel_limit=10,
+                            decel_limit=-10,
+                            max_steer_angle=0.25)
+
     controller.control(1, 0.1)
 
 if __name__ == "__main__":
