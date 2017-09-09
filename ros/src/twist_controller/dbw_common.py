[Con] Bugfixes... Fixed control call (wrong order of arguments), bugfix in calculation of cte, added some logging

ros/src/twist_controller/dbw_common.py
@@ -29,13 +29,8 @@ def get_cross_track_error(waypoints, pose):
 
     return cte
 
-def distance(self, waypoints, wp1, wp2):
-    dist = 0
-    dl = lambda a, b: math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2  + (a.z-b.z)**2)
-    for i in range(wp1, wp2+1):
-        dist += dl(waypoints[wp1].pose.pose.position, waypoints[i].pose.pose.position)
-        wp1 = i
-    return dist
+def distance(x1, y1, x2, y2):
+    return np.sqrt((x2-x1)**2 + (y2-y1)**2)
 
 def get_cross_track_error_from_frenet(waypoints, pose):
 