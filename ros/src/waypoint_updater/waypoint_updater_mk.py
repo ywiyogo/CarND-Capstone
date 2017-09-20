73547757039da8aee0e9d7c16d6e1959303e21c1
MK <martinkretzer@web.de>
2017-09-20 18:01:18 +0800

    Create waypoint_updater_mk.py

---
+++ ros/src/waypoint_updater/waypoint_updater_mk.py
@@ -0,0 +1,237 @@
+#!/usr/bin/env python
+
+
+import rospy
+
+from geometry_msgs.msg import PoseStamped
+from styx_msgs.msg import Lane, Waypoint, TrafficLightArray
+
+import scipy.spatial
+import math
+import numpy as np
+
+try:
+    from functools import lru_cache
+except ImportError:
+    from backports.functools_lru_cache import lru_cache
+
+
+LOOKAHEAD_WPS = 10  # 200 requires too much CPU.
+
+
+def constant_v_waypoints(waypoints, velocity, incremental=True):
+    final_waypoints = []
+
+    if incremental: forward_velocity = velocity
+    else:
+        forward_velocity = - velocity
+
+
+    x = [waypoint.pose.pose.position.x for waypoint in waypoints]
+    y = [waypoint.pose.pose.position.y for waypoint in waypoints]
+    delta_x = np.diff(x)
+    delta_y = np.diff(y)
+    delta_s = np.sqrt(np.square(delta_x) + np.square(delta_y))
+
+    last_waypoint = waypoints[0]
+    for index, waypoint in enumerate(waypoints[1:]):
+        velocity_x = forward_velocity * delta_x[index] / delta_s[index]
+        velocity_y = forward_velocity * delta_y[index] / delta_s[index]
+        last_waypoint.twist.twist.linear.x = velocity_x
+        last_waypoint.twist.twist.linear.y = velocity_y
+        final_waypoints.append(last_waypoint)
+
+        last_waypoint = waypoint
+
+    return final_waypoints
+
+'''
+def waypoints_under_lights(waypoints, lights, incremental=True):
+    look_ahead = 50
+    road_width = 30
+    length_zero_velocity = 8 
+
+    final_waypoints = []
+
+    if incremental:
+        forward_direction = 1
+    else:
+        forward_direction = - 1
+
+    x = [waypoint.pose.pose.position.x for waypoint in waypoints]
+    y = [waypoint.pose.pose.position.y for waypoint in waypoints]
+    delta_x = np.diff(x)
+    delta_y = np.diff(y)
+    delta_s = np.sqrt(np.square(delta_x) + np.square(delta_y))
+
+    last_waypoint = waypoints[0]
+    for index, waypoint in enumerate(waypoints[1:]):
+        direction_x = forward_direction * delta_x[index] / delta_s[index]
+        direction_y = forward_direction * delta_y[index] / delta_s[index]
+
+        longitudinal_road_vector = np.array([direction_x, direction_y])
+        lateral_road_vector = np.array([- direction_y, direction_y])
+
+        ratio = 1
+        x_waypoint = last_waypoint.pose.pose.position.x
+        y_waypoint = last_waypoint.pose.pose.position.y
+
+        for light in lights:
+            relative_position = np.array([light.pose.pose.position.x - x_waypoint,
+                                          light.pose.pose.position.y - y_waypoint])
+
+            longitudinal_position = relative_position.dot(longitudinal_road_vector)
+            lateral_position = relative_position.dot(lateral_road_vector)
+
+            if (longitudinal_position >= 0 and light.state == 0
+                and longitudinal_position <= length_zero_velocity
+                and abs(lateral_position) <= road_width):
+                this_ratio = 0
+            elif (longitudinal_position >= 0 and light.state == 0
+                  and longitudinal_position <= look_ahead
+                  and abs(lateral_position) <= road_width):
+                this_ratio = ((longitudinal_position - length_zero_velocity)
+                         / (look_ahead - length_zero_velocity))
+            else:
+                this_ratio = 1
+
+            ratio = min(this_ratio, ratio)
+                
+        old_velocity_x = last_waypoint.twist.twist.linear.x
+        old_velocity_y = last_waypoint.twist.twist.linear.y
+        last_waypoint.twist.twist.linear.x = old_velocity_x * ratio
+        last_waypoint.twist.twist.linear.y = old_velocity_x * ratio
+        final_waypoints.append(last_waypoint)
+        last_waypoint = waypoint
+
+    final_waypoints.append(last_waypoint)
+
+    return final_waypoints
+'''
+
+def get_kd_tree(waypoints):
+    if get_kd_tree.kd_tree == None:
+        waypoint_coordinates = [[waypoint.pose.pose.position.x,
+                                 waypoint.pose.pose.position.y] for waypoint in waypoints]
+        get_kd_tree.kd_tree = scipy.spatial.KDTree(waypoint_coordinates)
+        get_kd_tree.waypoint_coordinates = waypoint_coordinates
+        return get_kd_tree.kd_tree, get_kd_tree.waypoint_coordinates
+    else:
+        return get_kd_tree.kd_tree, get_kd_tree.waypoint_coordinates
+get_kd_tree.kd_tree = None
+get_kd_tree.waypoint_coordinates = None
+
+
+def get_closest_index_behind(waypoints, pose, incremental=True):
+
+
+    waypoints_kd_tree, waypoint_coordinates = get_kd_tree(waypoints)
+
+    pose_coordinates = [pose.pose.position.x, pose.pose.position.y]
+    _, index = waypoints_kd_tree.query(pose_coordinates)
+
+    next_index = (index + 1) % len(waypoints)
+    this_position = np.array(waypoint_coordinates[index])
+    next_position = np.array(waypoint_coordinates[next_index])
+    positive_vector = (this_position - next_position if incremental
+                       else this_position - next_position)
+    relative_position = np.array(pose_coordinates) - this_position
+    if (positive_vector.dot(relative_position) >= 0):
+        return index
+    else:
+        adjusted_index = index - 1 if incremental else index + 1
+        return index % len(waypoints)
+
+
+class WaypointUpdater(object):
+
+    def __init__(self):
+        rospy.init_node('waypoint_updater')
+        # queue_size: after receiving this number of messages, old messages will be deleted
+        # queue_size: all py-files seem to have queue_size=1, so I am using 1 here as well
+        # exception: the cpp-files in waypoint_follower have queue_size=10
+        self.waypoints = None
+        self.lights = []
+
+        rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb, queue_size=1)
+        rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb, queue_size=1)
+        rospy.Subscriber('/vehicle/traffic_lights', TrafficLightArray, self.traffic_cb, queue_size=1)
+
+        self.final_waypoints_pub = rospy.Publisher('/final_waypoints', Lane, queue_size=1)
+
+        rospy.spin()
+
+    def waypoints_cb(self, lane):
+        self.waypoints = lane.waypoints
+
+    def pose_cb(self, pose):
+
+        velocity = 10
+        if self.waypoints is not None and len(self.waypoints) > 0:
+            #rospy.logwarn('[waypoint_updater.py]: len(self.waypoints): %s', len(self.waypoints))
+            closest_wp_index = get_closest_index_behind(self.waypoints, pose)
+            waypoints_2laps = self.waypoints + self.waypoints
+            
+            # Get index in self.waypoints of nearest red traffic light:
+            ''' TODO: use existing function:
+            lights_closest_light_index = get_closest_index_behind(self.lights, pose, False)
+            wp_closest_light_index = get_closest_index_behind(self.waypoints, self.lights[lights_closest_light_index].pose, False)
+            '''
+            # Workaround for now with quick and dirty distance function:
+            closest_dist = 1000000.0
+            wp_closest_light_index = -1
+            for lights_i, waypoint in enumerate(self.lights):
+                if self.lights[lights_i].state != 2: # States:0=red, 1=yellow, 2=green, 4=unknown
+                    #rospy.logwarn('[waypoint_updater.py]: lights_i:%s /// waypoint: %s', lights_i, self.lights[lights_i])
+                    wp_redlight = get_closest_index_behind(self.waypoints, self.lights[lights_i].pose)
+                    if wp_redlight > closest_wp_index and self.distance( self.waypoints, closest_wp_index, wp_redlight ) < closest_dist:
+                        closest_dist = self.distance( self.waypoints, closest_wp_index, wp_redlight  )
+                        wp_closest_light_index = wp_redlight
+               # else:
+                    #rospy.logwarn('[waypoint_updater.py]: lights_i:%s /// waypoint: %s', lights_i, self.lights[lights_i])                    
+            
+            # Get distance between car and nearest red traffic light:
+            distance_car_redtl = self.distance(self.waypoints, closest_wp_index, wp_closest_light_index)
+            
+            lane = Lane()
+            if 36.0 < distance_car_redtl and distance_car_redtl < 76.0: # red traffic light ahead (26~length of the intersection-->we do not want to stop in the intersection --> 26+10 for braking=36)
+                rospy.logwarn('[waypoint_updater.py]: STOP: car:%s, tl:%s, distance:%s', closest_wp_index, wp_closest_light_index, distance_car_redtl)
+                lane.waypoints = constant_v_waypoints(waypoints_2laps[closest_wp_index:closest_wp_index+LOOKAHEAD_WPS],-velocity)
+            else: # no red traffic light or too late to stop anyway
+                rospy.logwarn('[waypoint_updater.py]: DRIVE: car:%s, tl:%s, distance:%s', closest_wp_index, wp_closest_light_index, distance_car_redtl)
+                lane.waypoints = constant_v_waypoints(waypoints_2laps[closest_wp_index:closest_wp_index+LOOKAHEAD_WPS],velocity)
+                #rospy.logwarn('[waypoint_updater.py]: lane.waypoints:%s', lane.waypoints)
+            
+            #lane.waypoints = waypoints_under_lights(lane.waypoints, self.lights)
+            
+            #self.final_waypoints_pub.publish(lane)
+
+
+    def traffic_cb(self, traffic_lights):
+        self.lights = traffic_lights.lights
+
+
+    '''
+    def obstacle_cb(self, msg):
+        # TODO: Callback for /obstacle_waypoint message. We will implement it later
+        pass
+
+    def get_waypoint_velocity(self, waypoint):
+        return waypoint.twist.twist.linear.x
+
+    def set_waypoint_velocity(self, waypoints, waypoint, velocity):
+        waypoints[waypoint].twist.twist.linear.x = velocity
+    '''
+    def distance(self, waypoints, wp1, wp2):
+        dist = 0
+        dl = lambda a, b: math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2  + (a.z-b.z)**2)
+        for i in range(wp1, wp2+1):
+            dist += dl(waypoints[wp1].pose.pose.position, waypoints[i].pose.pose.position)
+            wp1 = i
+        return dist
+    
+if __name__ == '__main__':
+    try:
+        WaypointUpdater()
+    except rospy.ROSInterruptException:
+        rospy.logerr('Could not start waypoint updater node.')
