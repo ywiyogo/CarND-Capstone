606d8ab5892788be05b239310c7d1b874b9a7856
Klemens Esterle <klemens.esterle@posteo.de>
2017-09-22 20:21:58 +0200

    [Con] got rid of unused filter

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -15,8 +15,6 @@ class TwistController(object):
         self.pid_steer = pid.PID(kp=0.1, ki=0.0001, kd=0.1,
                                  mn=-max_steer_angle, mx=max_steer_angle)
 
-        self.filter_steer = lowpass.LowPassFilter(tau=0.1, ts=1)
-
         self.last_timestamp = rospy.get_time()
 
     def control(self, cross_track_error):
@@ -31,16 +29,11 @@ class TwistController(object):
         duration = current_timestamp - self.last_timestamp
         self.last_timestamp = current_timestamp
 
-        #rospy.logwarn('Twist Controller: Sample time: %s', duration)
-
         #rospy.loginfo('Error cte: %s', cross_track_error)
         steering_angle = self.pid_steer.step(error=cross_track_error,
                                              sample_time=duration)
-        #rospy.loginfo('Steering angle PID output: %s', steering_angle)
-        # steering_angle_filtered = self.filter_steer.filt(steering_angle)
-        # rospy.loginfo('Steering angle PID output filtered: %s', steering_angle_filtered)
 
-        steering_out = steering_angle # steering_angle_filtered
+        steering_out = steering_angle
 
         return steering_out
 
@@ -48,11 +41,8 @@ class TwistController(object):
         """
         resets pid controllers
         """
-        #rospy.loginfo('reset', steering_angle)
 
         self.pid_steer.reset()
-        self.filter_steer.reset()
-
 
 def test_controller():
     """
